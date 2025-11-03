93e8e33978f4b6b7b3ce655f35f46b6c432244fa
pietroavola <pietroavola05@gmail.com>
2025-11-03 17:21:38 +0100

    Programma Commentato. Non testato con un run totale causa probabile blocco di Windows Defender che blocca l'accesso con il database. Nel file sono presenti dei print di Dubug. Mantenuto il modello MVC e gestione degli errori.

--- UI/controller.py
+++ UI/controller.py
@@ -28,4 +28,38 @@ class Controller:
         self._view.update()
 
     # Altre Funzioni Event Handler
-    # TODO
+    """Il controller mi permette di far interagire il Model (preleva i dati dal database) 
+    con la view (interfaccia utente)"""
+
+    def mostra_automobili(self):
+        #Idea: devo richiamare una funzione del model che mi restituisca una lista di oggetti Automobile
+        print("[DEBUG] sono dentro mostra_automobili nel controller")
+        lista_automobili = self._model.get_automobili()
+        #lista_auto è un oggetto di tipo flet List View
+        self._view.lista_auto.clean() #pulisca la List View
+        if lista_automobili is None:
+            self._view.show_alert("Errore nel recupero delle automobili.")
+        else:
+            for auto in lista_automobili:
+                print("[DEBUG]: Sono dentro il for per stampare le automobili")
+                self._view.lista_auto.append(ft.Text(str(auto))) #Aggiungo le auto dentro la list view
+            self._view.update() #aggiorno la pagina
+        #NOTA: convertire l'oggetto Auto in una stringa.
+        #      Aggiungo alla lista view un elemento ft.Text
+
+
+    def cerca_automobili_per_modello(self, input_modello_auto):
+        print("[DEBUG] Sono nel controller nella fz cerca automobili per modello")
+        # pulisco la lista prima di riempirla (altrimenti abbiamo visto a lezione si fa un append)
+        self._view.lista_auto_ricerca.clean()
+
+        # recupero le auto dal model (passando il testo del campo)
+        lista_ricercata = self._model.cerca_automobili_per_modello(input_modello_auto)
+
+        if lista_ricercata is None or len(lista_ricercata) == 0:
+            self._view.show_alert("Nessuna automobile trovata per questo modello.")
+        else:
+            for auto in lista_ricercata:
+                    self._view.lista_auto_ricerca.controls.append(ft.Text(str(auto))) #ricordando che lista ricercata è una lista di oggetti auto
+        self._view.update()
+
